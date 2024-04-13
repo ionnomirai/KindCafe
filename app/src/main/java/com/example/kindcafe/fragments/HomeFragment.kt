2a99a8f1d7fcafedf9c673374e86bda4264671eb
ionnomirai <kindbakaneko@gmail.com>
2024-04-13 10:27:01 -0700

    Added Home Fragment and joined to MainActivity:
    - changed activity_main.xml (root and other elements)
    - added Navigation library
    - added menus for drawer (side menu) and for toolbar
    - added NavGraph

---
+++ app/src/main/java/com/example/kindcafe/fragments/HomeFragment.kt
@@ -0,0 +1,40 @@
+package com.example.kindcafe.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.kindcafe.databinding.FragHomeBinding
+
+class HomeFragment: Fragment() {
+    /*---------------------------------------- Properties ----------------------------------------*/
+    private var _binding: FragHomeBinding? = null
+    private val binding
+        get() : FragHomeBinding{
+            return checkNotNull(_binding){
+                "Cannot access binding because it is null. Is the view visible"
+            }
+        }
+
+
+    /*---------------------------------------- Functions -----------------------------------------*/
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragHomeBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
