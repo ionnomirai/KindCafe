a20c816a3dc1414fd6381bd2f63046aebc13d759
ionnomirai <kindbakaneko@gmail.com>
2024-04-14 10:41:53 -0700

    Added new UI and simple interaction:
    - Added restriction - only Portrait mode
    - Added empty bottom menu
    - Added search icon
    - Added empty fragment (for further registration)
    - Set simple interact with toolbar, bottom navigation and navigationView (side navigation)

--- app/src/main/java/com/example/kindcafe/MainActivity.kt
+++ app/src/main/java/com/example/kindcafe/MainActivity.kt
@@ -2,13 +2,108 @@ package com.example.kindcafe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.MotionEvent
+import android.view.WindowManager
+import android.widget.Toast
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.kindcafe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    /*---------------------------------------- Properties ----------------------------------------*/
+
     private lateinit var binding: ActivityMainBinding
+
+    /*  AppBarConfiguration:
+    * - determining which drawerLayout to work with;
+    * - definition of the "Burger" button;
+    * - definition of top level destination;
+    * - control of the back button for further transitions. */
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    /* NavController - This will be the object that keeps track of the current navigation position
+    * among the navGraph. It also changes fragments in NavHostFrament*/
+    private lateinit var navController: NavController
+
+    /*---------------------------------------- Functions -----------------------------------------*/
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        /* Connecting my custom Action Bar */
+        setSupportActionBar(binding.tbMain)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fcv_main) as NavHostFragment
+
+        navController = navHostFragment.navController
+
+        appBarConfiguration = AppBarConfiguration(
+            navGraph = navController.graph,
+            drawerLayout = binding.lDrawLayoutMain
+        )
+
+        setupNavigationMenu(navController)
+        setupActionBar(navController, appBarConfiguration)
+        setupBottomNavMenu(navController)
+
+        /* Full Screen */
+        setFullScreen()
+
+
+    }
+
+    /* Connecting NavigationView (sliding panel) to navController so that you can navigate. */
+    private fun setupNavigationMenu(navController: NavController){
+        binding.nvLeft.setupWithNavController(navController)
+    }
+
+    /* Connecting NavigationView (sliding panel) to toolbar (accessed via appBarConfiguration). This does the following:
+    *  - shows the current location in place title (toolbar);
+    *  - shows the back button when we are not in the top destination;
+    *  - shows the "burger" button when we are in the top destination. */
+    private fun setupActionBar(navController: NavController, appBarConfiguration: AppBarConfiguration){
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    /* Handling back button click */
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration)
+    }
+
+    private fun setupBottomNavMenu(navController: NavController) {
+        binding.bnvMain.setupWithNavController(navController)
+    }
+
+    private fun setFullScreen(){
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, binding.root).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.systemBars())
+            controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu_general, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.itbSearch){
+            Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
---
+++ app/src/main/java/com/example/kindcafe/fragments/RegistrationFragment.kt
@@ -0,0 +1,40 @@
+package com.example.kindcafe.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.kindcafe.databinding.FragHomeBinding
+import com.example.kindcafe.databinding.FragRegistrationBinding
+
+class RegistrationFragment: Fragment() {
+    private var _binding: FragRegistrationBinding? = null
+    private val binding
+        get() : FragRegistrationBinding {
+            return checkNotNull(_binding){
+                "Cannot access binding because it is null. Is the view visible"
+            }
+        }
+
+
+    /*---------------------------------------- Functions -----------------------------------------*/
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragRegistrationBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
